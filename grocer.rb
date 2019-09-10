2485640f7abb287d8fa45013b8cec9b7f72f7281
Rei M <r.clarkemitchell@gmail.com>
2019-09-10 04:54:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,49 @@
 def consolidate_cart(cart)
-  # code here
+new_hash = {}
+cart.each do |items|
+  items.each do |groceryName, groceryV|
+    if new_hash[groceryName]
+    new_hash[groceryName][:count] += 1
+    else
+    new_hash[groceryName] = groceryV
+    new_hash[groceryName][:count] = 1
+  end
+end
+end
+new_hash
 end
 
-def apply_coupons(cart, coupons)
-  # code here
+# consolidate_cart([
+#   {"AVOCADO" => {:price => 3.00, :clearance => true }},{"AVOCADO" => {:price => 3.00, :clearance => true }},{"KALE" => {:price => 3.00, :clearance => false}}])
+
+def apply_coupons(cart, coupons = nil)
+  if coupons.length == 0 || coupons == nil
+  return cart
+  elsif cart.size == 0
+     return cart
+  end
+new_hash = {}
+coupons.each do |coupon|
+cart.each do |itemName, itemValue|
+ if coupon[:item] == itemName && cart[itemName][:count] >= coupon[:num]
+if new_hash["#{itemName} W/COUPON"]
+new_hash["#{itemName} W/COUPON"][:count] += 2
+else
+new_hash["#{itemName} W/COUPON"] = {:price => coupon[:cost] / coupon[:num], :clearance => cart[itemName][:clearance], :count => coupon[:num] }
 end
+cart[itemName][:count] = cart[itemName][:count] - coupon[:num]
+
+end
+new_hash[itemName] = cart[itemName]
+end
+
+end
+
+new_hash
+
+end
+
+
 
 def apply_clearance(cart)
   # code here
