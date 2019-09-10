31fca1cd02564c44fcdb4102d2c4b88c91a91809
Rei M <r.clarkemitchell@gmail.com>
2019-09-10 05:15:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,21 +30,23 @@ else
 new_hash["#{itemName} W/COUPON"] = {:price => coupon[:cost] / coupon[:num], :clearance => cart[itemName][:clearance], :count => coupon[:num] }
 end
 cart[itemName][:count] = cart[itemName][:count] - coupon[:num]
-
 end
 new_hash[itemName] = cart[itemName]
 end
-
 end
-
 new_hash
   end
 
+  def apply_clearance(cart)
+  cart.each do |item, v|
+  	if cart[item][:clearance] == true
+  	discount20 = (cart[item][:price] * 0.20)
+  	cart[item][:price]=  cart[item][:price] - discount20
+  	end
 
-
-def apply_clearance(cart)
-  # code here
-end
+  end
+  cart
+  end
 
 def checkout(cart, coupons)
   # code here
