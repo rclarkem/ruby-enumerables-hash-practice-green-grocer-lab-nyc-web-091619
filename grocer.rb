5da3c945e3a71ab8fb7b1c6c4184a4a8fad564a7
Rei M <r.clarkemitchell@gmail.com>
2019-09-10 05:51:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -53,6 +53,7 @@ new_hash
   end
 cart = consolidate_cart(cart)
 coupons_for_items = apply_coupons(cart, coupons)
+clearance_items = apply_clearance(cart, coupons_for_items)
   total_price = cart.reduce(0) do |memo, (key, val)|
   p memo + val[:price]
   end
